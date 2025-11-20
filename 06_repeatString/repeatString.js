2932137096267ef89d52f73d4160e49a1dba23cb
Charlie Gainey <cgainey@bates.edu>
2025-11-20 15:21:17 -0800

    completed exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    stringRepeat = ''
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++) {
+        stringRepeat += string;
+    }
+    return stringRepeat;
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
