8bfdef8bc40d593cc258df5a399e98cef1eafe7d
Charlie Gainey <cgainey@bates.edu>
2025-11-21 14:26:00 -0800

    completed exercises for loops and arrays chapter

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(item => {
+        if (args.includes(item)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    })
 };
 
 // Do not edit below this line
